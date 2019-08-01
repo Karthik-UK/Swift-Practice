b9b425167b744fa19b5db8ef643eeec7f6b2a89c
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-08-01 14:33:21 +0530

    bsearch

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,4 +10,44 @@ for (index, value) in shoppingList.enumerated() {
     print("Item \(index + 1): \(value)")
 }
 
-var a = [10,20,30]
+
+
+
+
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+//Binary search
+func Bin(arr:[Int],ser:Int)
+{var l = 0
+    var r = arr.count
+    var mid = 0
+    while(l<=r)
+    {
+        mid = (l + r - 1)/2
+        if mid == ser
+        {
+            print("Element found ")
+            break
+        }
+        else
+        {
+            if ser>=mid
+            {l = mid}
+            else
+            {r = mid}
+        }
+    
+        
+    
+   
+    
+}
+}
+
+Bin(arr:[1,2,3,4,5,6,7],ser :3documenrts)
+
+
