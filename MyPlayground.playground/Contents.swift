05c7af3c3f0091abaed0a17ee4938eebcc6807c2
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-08-01 10:45:19 +0530

    First commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+var st = [1,2,3,4]
+print(st)
+var shoppingList = ["a","b","c"]
+//MySyrup.remove(at: 1)
+//print(MySyrup)
+for (index, value) in shoppingList.enumerated() {
+    print("Item \(index + 1): \(value)")
+}
+
+var a = [10,20,30]
